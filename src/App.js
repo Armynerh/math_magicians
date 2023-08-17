b5338e03aea1786f57b0f86830329acd4f57c6a1
Armynerh <aimenah@outlook.com>
2023-08-17 15:34:20 +0100

    modified App.js to include files event hanldlers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import React, { useState } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
+import calculate from './logic/calculate';
 
 function App() {
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (buttonName) => {
+    const newData = calculate(calculatorData, buttonName);
+    if (buttonName === '=') {
+      newData.next = null;
+      newData.operation = null;
+    }
+
+    setCalculatorData(newData);
+  };
+
   return (
     <div className="App">
-      <Calculator>
-        <div className="zero-div"><div className="num-item  top-num">0</div></div>
-      </Calculator>
+      <Calculator data={calculatorData} onButtonClick={handleButtonClick} />
     </div>
   );
 }
+
 export default App;
